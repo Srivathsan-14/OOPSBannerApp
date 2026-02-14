2eae63e97f83f28582870c32b76018c603b49139
Srivathsan S <rs5194@srmist.edu.in>
2026-02-14 22:04:38 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * OOPS Banner App - UC1
+ * This class prints a simple OOPS message to the console.
+ * * @author Srivathsan
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // Simple print statement
+        System.out.println("OOPS");
+    }
+}
